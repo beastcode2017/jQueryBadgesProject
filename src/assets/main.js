dc35b0335f964f07af8c7525e7e438b9f6ffc2c1
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 17:16:23 -0500

    Try another one

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,8 +10,7 @@ $.ajax({
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
-      $('.course h3:last').after("<img />");
-      $('.course h3 img:last').attr('src', cbadge);
+      $('.course h3:last').after("<img "+"'"+cbadge"'"+"/>");
       $('.course h3 img:last').after("<a target='_blank'>See Course</a>").addClass("btn btn-primary");
     });
   }
