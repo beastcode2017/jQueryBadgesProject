3038a1cc4b3c826a6ace698a5a6742d579652c49
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 15:20:28 -0500

    Use msg to fix error

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,11 +8,10 @@ $.ajax({
     response.courses.completed.forEach(function(course){
       var msg = $('<div></div>');
       msg.addClass('course');
-
+      msg = $('#badges').append(msg);
       msg.append('<h3></h3>').text(course.title);
-      msg.after('<img/>').attr('src','course.badge');
-      msg.after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
-      $('#badges').append(msg);
+      $('.course h3:last').after('<img/>').attr('src','course.badge');
+      $('.course h3 img:last').after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
   }
 });
