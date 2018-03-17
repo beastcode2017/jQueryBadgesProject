0e1f3f6a617222319b80e7950e193e49e9b0c10e
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 17:01:09 -0500

    Make more changes

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,8 @@ $.ajax({
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
-      $('.course h3:last').after('<img/>').attr('src','course.badge');
-      $('.course h3 img:last').after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
+      $('.course h3:last').after("<img src="+course.badge+"/>");
+      $('.course h3 img:last').after("<a href='course.url' target='_blank'>See Course</a>").addClass("btn btn-primary");
     });
   }
 });
