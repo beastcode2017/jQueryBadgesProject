1d0609422ef0d34b213a3cbf70308083c731d279
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 17:12:34 -0500

    Just a message

--- assets/main.js
+++ assets/main.js
@@ -6,11 +6,12 @@ $.ajax({
   dataType:'jsonp',
   success: function(response){
     response.courses.completed.forEach(function(course){
+      var cbadge = course.badge;
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
       $('.course h3:last').after("<img />");
-      $('.course h3 img:last').attr('src', 'course.badge');
+      $('.course h3 img:last').attr('src', cbadge);
       $('.course h3 img:last').after("<a target='_blank'>See Course</a>").addClass("btn btn-primary");
     });
   }
