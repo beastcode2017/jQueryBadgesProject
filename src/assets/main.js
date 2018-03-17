a96bbf4e5757a50f96dba071afca17c590842384
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 14:35:01 -0500

    Insert variable

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,7 +8,8 @@ $.ajax({
     response.courses.completed.forEach(function(course){
       var msg = $('<div></div>');
       msg.addClass('course');
-      $('#badges').append(msg).append('<h3></h3>').text(course.title);
+      var placard = $('#badges').append(msg);
+      placard.append('<h3></h3>').text(course.title);
       $('.course:last').append('<img/>').attr('src','course.badge');
       $('.course:last').append("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
