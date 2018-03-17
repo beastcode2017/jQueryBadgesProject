62b20cab866a0583711aa92d90e0175f6aa9bdcf
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 17:20:48 -0500

    Anonther one

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,7 @@ $.ajax({
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
-      $('.course h3:last').after("<img src="+"'"+course.badge"'"+"/>");
+      $('.course h3:last').after("<img src="+"'"+course.badge+"'"+"/>");
       $('.course h3 img:last').after("<a target='_blank'>See Course</a>").addClass("btn btn-primary");
     });
   }
