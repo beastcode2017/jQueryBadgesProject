e085101a43d26a520cad50348fed0b1355c23836
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 14:12:51 -0500

    Fix h3

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,8 @@ $.ajax({
       var msg = $('<div></div>')
       msg.addClass('course');
       $('#badges').append(msg);
-      $('.course').append('<h3>'+course.title+'</h3>');
+      $('.course').append('<h3></h3>');
+      $('.course h3').text(course.title);
       $('.course').append('<img/>').attr('src','course.badge');
       $('.course').append("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
