65ec815fcea3e48294d1a03f439601dddd9b63ef
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 17:18:50 -0500

    Forgot src

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,11 +6,10 @@ $.ajax({
   dataType:'jsonp',
   success: function(response){
     response.courses.completed.forEach(function(course){
-      var cbadge = course.badge;
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
-      $('.course h3:last').after("<img "+"'"+cbadge"'"+"/>");
+      $('.course h3:last').after("<img src="+"'"+course.badge"'"+"/>");
       $('.course h3 img:last').after("<a target='_blank'>See Course</a>").addClass("btn btn-primary");
     });
   }
