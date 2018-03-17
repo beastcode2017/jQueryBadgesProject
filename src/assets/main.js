0050fccb3a495b7f00da846556450cd33f2f345d
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 14:31:12 -0500

    Chaining methods

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,9 +8,7 @@ $.ajax({
     response.courses.completed.forEach(function(course){
       var msg = $('<div></div>');
       msg.addClass('course');
-      $('#badges').append(msg);
-      $('.course:last').append('<h3></h3>');
-      $('h3').text(course.title);
+      $('#badges').append(msg).append('<h3></h3>').text(course.title);
       $('.course:last').append('<img/>').attr('src','course.badge');
       $('.course:last').append("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
