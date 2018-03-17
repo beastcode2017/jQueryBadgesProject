479f4afa9b762eefb3d638ba99601c0fe1e03397
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 14:25:23 -0500

    Need to generate h3 every time

--- assets/main.js
+++ assets/main.js
@@ -6,13 +6,13 @@ $.ajax({
   dataType:'jsonp',
   success: function(response){
     response.courses.completed.forEach(function(course){
-      var msg = $('<div></div>')
+      var msg = $('<div></div>');
       msg.addClass('course');
       $('#badges').append(msg);
-      $('.course').append('<h3></h3>');
-      $('.course h3').text(course.title);
-      $('.course').append('<img/>').attr('src','course.badge');
-      $('.course').append("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
+      $('.course:last').append('<h3></h3>');
+      $('h3').text(course.title);
+      $('.course:last').append('<img/>').attr('src','course.badge');
+      $('.course:last').append("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
   }
 });
