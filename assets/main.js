c3b2b044087a0863538191c562553dddd8498d35
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 15:02:44 -0500

    Fix number 6

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,8 @@ $.ajax({
     response.courses.completed.forEach(function(course){
       var msg = $('<div></div>');
       msg.addClass('course');
-      $('#badges').append(msg).append('<h3></h3>').text(course.title);
+      $('#badges').append(msg);
+      $('.course:last').append('<h3></h3>').text(course.title);
       $('.course h3:last').after('<img/>').attr('src','course.badge');
       $('.course h3 img:last').after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
