2bf45c3db1fee50e5936461027730f550476079a
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 16:51:25 -0500

    Still trying

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,9 +6,9 @@ $.ajax({
   dataType:'jsonp',
   success: function(response){
     response.courses.completed.forEach(function(course){
-      var title = course.title;
+      var title = new String(course.title);
       $('#badges').append("<div class='course'></div>");
-      $('.course').append('<h3></h3>').text('course.title');
+      $('.course:last').append('<h3></h3>').text(title);
       $('.course h3:last').after('<img/>').attr('src','course.badge');
       $('.course h3 img:last').after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
