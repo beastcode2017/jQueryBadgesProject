9c467226d92fc0699e7ef96b032212f90bc77910
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 15:26:55 -0500

    delete variable

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,10 +6,7 @@ $.ajax({
   dataType:'jsonp',
   success: function(response){
     response.courses.completed.forEach(function(course){
-      var msg = $('<div></div>');
-      msg.addClass('course');
-      msg = $('#badges').append(msg);
-      msg.append('<h3></h3>').text(course.title);
+      $('#badges').append("<div class='course'></div>").append('<h3></h3>').text(course.title);
       $('.course h3:last').after('<img/>').attr('src','course.badge');
       $('.course h3 img:last').after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
