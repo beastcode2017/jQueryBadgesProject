debac5268eaf055400ca0dd7b5957bb92b483668
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 16:45:02 -0500

    Fix text method

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,7 @@ $.ajax({
     response.courses.completed.forEach(function(course){
       var title = course.title;
       $('#badges').append("<div class='course'></div>");
-      $('.course').prepend('<h3></h3>').text(title);
+      $('.course').append('<h3></h3>').text('course.title');
       $('.course h3:last').after('<img/>').attr('src','course.badge');
       $('.course h3 img:last').after("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
     });
