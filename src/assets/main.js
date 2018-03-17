c252ad2da24252cf7a512b3fb2e94e25afdf6931
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 13:56:36 -0500

    Add jQuery

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,23 @@
 $(function() {
 
   // your code will go here
+$.ajax({
+  url: 'https://www.codeschool.com/users/Kingbreaker.json',
+  dataType:'jsonp',
+  success: function(response){
+    response.courses.completed.forEach(function(course){
+      var msg = $('<div></div>')
+      msg.addClass('course');
+      $('#badges').append(msg);
+      $('.course').append('<h3>'+course.title+'</h3>');
+      $('.course').append('<img/>').attr('src','course.badge');
+      $('.course').append("<a href='course.url' target='_blank'></a>").addClass("btn btn-primary").text("See Course");
+    });
 
+
+  },
+
+
+  }
+})
 });
