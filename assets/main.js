6f157dbd17f4cb1c990c400a112fc0445a1ef51c
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-17 17:07:33 -0500

    Test course badge

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,9 @@ $.ajax({
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
-      $('.course h3:last').after("<img src="+course.badge+"/>");
-      $('.course h3 img:last').after("<a href='course.url' target='_blank'>See Course</a>").addClass("btn btn-primary");
+      $('.course h3:last').after("<img />");
+      $('.course h3 img:last').attr('src', 'course.badge');
+      $('.course h3 img:last').after("<a target='_blank'>See Course</a>").addClass("btn btn-primary");
     });
   }
 });
