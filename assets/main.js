c4139c6da50b460161f5e146904e96a36861f51c
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-18 16:08:36 -0500

    Try this

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,7 @@ $.ajax({
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
       $('.course h3:last').after("<img src="+"'"+course.badge+"'"+"/>");
-      $('.course h3 img:last').append("<a>See Course</a>")
+      $('.course h3 img:last').append('<a></a>');
       $('.course h3 img a:last').addClass("btn btn-primary");
       $('.course h3 img a:last').attr('target','_blank');
     });
