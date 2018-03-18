b53071e369d86ac94673204f201471cb6e667a55
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-18 16:58:26 -0500

    Need help

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,13 +5,18 @@ $.ajax({
   url: 'https://www.codeschool.com/users/Kingbreaker.json',
   dataType:'jsonp',
   success: function(response){
-  for (var i=0; i < response.courses.completed.length; i++){
+    response.courses.completed.forEach(function(course){
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
-      $('.course h3:last').append(response.courses.completed[i].title);
-      $('.course h3:last').after("<img src="+"'"+response.courses.completed[i].badge+"'"+"/>");
-      $('a').appendTo($('.course:last'));
-    }
+      $('.course h3:last').append(course.title);
+      $('.course h3:last').after("<img src="+"'"+course.badge+"'"+"/>");
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target:'_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo('.course:last');
+    });
   }
 });
 })();
