0b1ea56838a7ff94150e1e621eac794a49f7cb58
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-18 16:37:02 -0500

    Change to for loop

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,13 +5,13 @@ $.ajax({
   url: 'https://www.codeschool.com/users/Kingbreaker.json',
   dataType:'jsonp',
   success: function(response){
-    response.courses.completed.forEach(function(course){
+  for (var i=0; i < response.courses.completed.length; i++){
       $('#badges').append("<div class='course'></div>");
       $('.course:last').append('<h3></h3>');
       $('.course h3:last').append(course.title);
       $('.course h3:last').after("<img src="+"'"+course.badge+"'"+"/>");
       $('.course h3 img:last').add("a");
-    });
+    }
   }
 });
 })();
