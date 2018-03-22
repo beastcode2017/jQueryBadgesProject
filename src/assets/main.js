1b8a8f7153798ffb6de88da021a1e374fc052fa7
beastcode2017 <beastcode2017@users.noreply.github.com>
2018-03-22 08:30:02 -0500

    Add curly brace

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,19 +6,26 @@ $.ajax({
   dataType:'jsonp',
   success: function(response){
     addCourses(response.courses.completed);
+    }
   });
 function addCourses(courses){
+
   var $badges = $('#badges');
+
   courses.forEach(function(course){
+
     var $course = $('<div />'), {
       'class':'course'
     }).appendTo($badges);
+
     $('<h3 />'), {
       text:course.title
     }).appendTo($course);
+
     $('<img />'), {
       src:course.badge
     }).appendTo($course);
+
     $('<a />'), {
       'class':'btn btn-primary',
       target:'blank',
@@ -27,4 +34,4 @@ function addCourses(courses){
     }).appendTo($course);
   })
 }
-})();
+});
